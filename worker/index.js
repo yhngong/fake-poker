b02d65744c1284a1a3f06a7fdd01dd735f57a2a8
Yolet Gong <yoletgong@YohandeiMac.local>
2026-09-13 16:58:05 -0700

    Configure static assets in public/ and suppress devtools probe to unify frontend and backend on port 8787

--- worker/index.js
+++ worker/index.js
@@ -904,6 +904,11 @@ export default {
       return new Response(null, { headers: corsHeaders });
     }
 
+    // Ignore Chrome DevTools probe
+    if (url.pathname.startsWith('/.well-known/')) {
+      return new Response(null, { status: 204 });
+    }
+
     // Favicon handler
     if (url.pathname === '/favicon.ico') {
       const svgSpade = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">♠</text></svg>`;
